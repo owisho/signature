fa1e8ca4ca17aed6d8792629c99a86b906e66599
owisho <owisho2012@126.com>
2017-01-09 14:54:54 +0800

    提价完整例子

--- src/main/java/per/owisho/signature/security/ex2/Example2a.java
+++ src/main/java/per/owisho/signature/security/ex2/Example2a.java
@@ -7,11 +7,10 @@ import per.owisho.signature.security.stranger.Stranger;
 public class Example2a {
 
 	public static void main(String[] args) {
-		TextFileDisplayer tfd = new TextFileDisplayer("question.txt");
+		TextFileDisplayer tfd = new TextFileDisplayer("answer.txt");
 		Friend friend = new Friend(tfd,true);
 		Stranger stranger = new Stranger(friend,true);
 		stranger.doYourThing();
-		
 	}
 	
 }
